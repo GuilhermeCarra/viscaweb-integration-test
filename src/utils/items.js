6c16cebafe45e312c531d8a30a70d42e3ea5be35
Guilherme Carra <gui_gc@hotmail.com>
2020-11-25 18:40:05 +0100

    Add mock data for betting webpages

---
+++ src/utils/items.js
@@ -0,0 +1,70 @@
+import Logo1 from './logos/Logo1.png';
+import Logo2 from './logos/Logo2.png';
+import Logo3 from './logos/Logo3.png';
+import Logo4 from './logos/Logo4.png';
+import Logo5 from './logos/Logo5.png';
+
+const items = [
+    {
+        id: 1,
+        name: 'Sports Interaction',
+        rank: 1,
+        logo: Logo1,
+        rating: 5,
+        exclusive: true,
+        offer: 'Bonus',
+        offerDetailed: 'Bonus up to $200',
+        value: '$200',
+        page: 'https://www.sportsinteraction.com/'
+    },
+    {
+        id: 2,
+        name: 'Power Play Sports',
+        rank: 2,
+        logo: Logo2,
+        rating: 5,
+        exclusive: true,
+        offer: 'Bonus',
+        offerDetailed: 'Bonus up to $200',
+        value: '$200',
+        page: 'https://www.powerplay.com/'
+    },
+    {
+        id: 3,
+        name: 'Betway',
+        rank: 3,
+        logo: Logo3,
+        rating: 5,
+        exclusive: false,
+        offer: 'Bonus',
+        offerDetailed: '100% Sign Up Bonus Up to $200',
+        value: '$200',
+        page: 'http://www.betway.com'
+    },
+    {
+        id: 4,
+        name: 'Spin Sports',
+        rank: 4,
+        logo: Logo4,
+        rating: 4,
+        exclusive: true,
+        offer: 'Free Bet',
+        offerDetailed: 'Up to $500 Free Bet',
+        value: '$500',
+        page: 'https://www.spinsports.com/es/'
+    },
+    {
+        id: 5,
+        name: 'Bodog',
+        rank: 5,
+        logo: Logo5,
+        rating: 3,
+        exclusive: true,
+        offer: 'Bonus',
+        offerDetailed: '100% Sign Up Bonus Up to $200',
+        value: '$200',
+        page: 'http://www.bodog.com'
+    },
+]
+
+export default items;
