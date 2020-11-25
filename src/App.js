c9e253903b087686c735c05b047acc3213ed2a3f
Guilherme Carra <gui_gc@hotmail.com>
2020-11-25 18:38:11 +0100

    Add widget for the main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import RankingWidget from './components/RankingWidget/RankingWidget'
+import items from './utils/items'
+import './App.scss';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RankingWidget
+        items={items}
+      />
     </div>
   );
 }
